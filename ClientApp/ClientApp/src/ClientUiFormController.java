4e54369ef02d0b6a8c2b466760cd88371d883f97
Imesh <imeshisuranga00@gmail.com>
2023-08-01 00:42:06 +0530

    Implemented ServerUiFormController.

--- ClientApp/ClientApp/src/ClientUiFormController.java
+++ ClientApp/ClientApp/src/ClientUiFormController.java
@@ -1,2 +1,50 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ClientUiFormController {
+    public TextArea txtshowArea;
+    public TextField txtmsgToSend;
+
+
+    private static Socket socket = null;
+    private static DataInputStream dataInputStream = null;
+    private static DataOutputStream dataOutputStream = null;
+
+    private void initServer(){
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",5000);
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                String message = "";
+                while(!message.equals("exit")){
+                    message = dataInputStream.readUTF();
+                    txtshowArea.setText(txtshowArea.getText()+"\n Server :\t"+message);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void initialize(){
+        initServer();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        String messageToSend = "";
+        messageToSend = txtmsgToSend.getText().trim();
+        dataOutputStream.writeUTF(messageToSend);
+
+    }
 }
--- ServerApp/ServerApp/src/ServerUiFormController.java
+++ ServerApp/ServerApp/src/ServerUiFormController.java
@@ -1,2 +1,51 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerUiFormController {
+    public TextArea txtshowArea;
+    public TextField txtmsgToSend;
+
+    private static ServerSocket serverSocket = null;
+    private static Socket socket = null;
+    private static DataInputStream dataInputStream = null;
+    private static DataOutputStream dataOutputStream = null;
+
+    private void initServer(){
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(5000);
+                socket = serverSocket.accept();
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                String message = "";
+                while(!message.equals("exit")){
+                    message = dataInputStream.readUTF();
+                    txtshowArea.setText(txtshowArea.getText()+"\n Client :\t"+message);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void initialize(){
+        initServer();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        String messageToSend = "";
+        messageToSend = txtmsgToSend.getText().trim();
+        dataOutputStream.writeUTF(messageToSend);
+
+    }
 }
